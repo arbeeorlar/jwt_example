e665fedb8364f07898b11ed5e1f55e936d38435c
arbeeorlar <biola.gold@gmail.com>
2025-04-08 23:23:25 +0100

    grouping the api

---
+++ controllers/middleware.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"github.com/arbeeorlar/jwt-example/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func AuthenticationMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing authentication token"})
+			c.Abort()
+			return
+		}
+
+		// The token should be prefixed with "Bearer "
+		tokenParts := strings.Split(tokenString, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authentication token"})
+			c.Abort()
+			return
+		}
+
+		tokenString = tokenParts[1]
+
+		claims, err := utils.ParseToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authentication token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", claims.User.ID)
+		c.Next()
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,19 @@ import (
 )
 
 func AuthRoutes(router *gin.Engine) {
-	router.POST("/login", controllers.LoginController)
-	router.GET("/premium", controllers.PremiumController)
-	router.POST("/signup", controllers.SignUpController)
-	router.GET("/signout", controllers.SignOutController)
-	router.GET("/home", controllers.HomeController)
+
+	public := router.Group("/auth")
+	{
+		public.POST("/login", controllers.LoginController)
+		public.POST("/signup", controllers.SignUpController)
+	}
+
+	private := router.Group("/user")
+	private.Use(controllers.AuthenticationMiddleware())
+	{
+		private.GET("/premium", controllers.PremiumController)
+		private.GET("/signout", controllers.SignOutController)
+		private.GET("/home", controllers.HomeController)
+	}
+
 }
